fd6d1bdb914b090348d801821a796df2bc0c6f72
Krystian Wojtas <krystian.wojtas@gmail.com>
2012-07-21 22:24:33 +0200

    fat commit; remove some generic moudles about dividing clock - instead of them one ModClk; spi module looks much better;

--- dac/dacLTC2624_behav.v
+++ dac/dacLTC2624_behav.v
@@ -79,55 +79,45 @@ module dacLTC2624behav
 	end
 	
 	
-	reg [31:0] indacshiftreg;
-	wire [11:0] data = indacshiftreg[15:4];
-	wire [3:0] address = indacshiftreg[19:16];
-	wire [3:0] command = indacshiftreg[23:20];
+	reg [31:0] conf;
+	wire [11:0] data = conf[15:4];
+	wire [3:0] address = conf[19:16];
+	wire [3:0] command = conf[23:20];
 	//rejestr licznika jest poszerzony o jeden bit w celu wykrycia bledu odbierania zbyt wielu bitow
-	reg [5:0] indacshiftregidx; 
+	reg [5:0] conf_idx; 
 	
 	//Blok odbioru danych z linii SPI_MOSI
-	//Reset ukladu (chwilowym obnizeniem DAC_CLR) zeruje glowny rejestr konfigurujacy daca indacshiftreg oraz jego licznik indacshiftregidx
-	//Obnizenie linii DAC_CS powoduje aktywowanie odbioru bitow i doklejanie ich do rejestru przesuwnego indacshiftreg w takt zegara SPI_SCK
-	//Licznik indacshiftregidx zerowany jest gdy transmisja nie jest aktywna.
+	//Reset ukladu (chwilowym obnizeniem DAC_CLR) zeruje glowny rejestr konfigurujacy daca conf oraz jego licznik conf_idx
+	//Obnizenie linii DAC_CS powoduje aktywowanie odbioru bitow i doklejanie ich do rejestru przesuwnego conf w takt zegara SPI_SCK
+	//Licznik conf_idx zerowany jest gdy transmisja nie jest aktywna.
 	//Gdy jest aktywna zwiekszany jest o jeden w takt zegara, co pokazuje aktulna liczbe odebranych bitow
-	always @(negedge DAC_CS or negedge DAC_CLR or negedge SPI_SCK) begin
+	always @(negedge DAC_CLR or negedge SPI_SCK) begin
 		if(~DAC_CLR) begin
-			indacshiftreg <= 32'd0;
-			indacshiftregidx <= 6'd0;
+			conf <= 32'd0;
+			conf_idx <= 6'd0;
 		end else
 			if(DAC_CS) begin
-				indacshiftregidx <= 6'd0;
+				conf_idx <= 6'd0;
 			end else begin
-				indacshiftreg <= { indacshiftreg[30:0], SPI_MOSI };
-				indacshiftregidx <= indacshiftregidx + 1;
+				conf <= { conf[30:0], SPI_MOSI };
+				conf_idx <= conf_idx + 1;
 			end
 	end
+		
 	
 	//Blok sprawdza czy ilosc odebranych bitow wynosi dokladnie 32
 	//Jesli sie zgadza ustawia flage received32bits
 	//Flaga ta jest sprawdzana w kolejnym bloku w momencie podniesienia linii DAC_CS, co sygnalizuje koniec transmisji
 	//Na tej podstwie moga zostac zglaszane bledy jesli przeslanych bitow jest zbyt malo
-	reg received32bits;
-	always @(negedge SPI_SCK)
-		if(indacshiftregidx == 32)
-			received32bits <= 1'b1;
-		else
-			received32bits <= 1'b0;
+	wire received32bits = (conf_idx == 32);
 	
 	//Blok wykrywa przeslanie zbyt duzej liczby bitow do daca
 	//Flaga receivedtoomanybits jest wygaszana w momencie aktywowania odbioru danych obnizeniem linii DAC_CS
 	//Flaga jest podnoszona jesli liczba odebranych bitow przekroczy prawidlowa ilosc 32.
 	//Stan podniesionej flagi jest utrzymywany do momentu aktywowania odbioru nowej danej.
-	//W tym czasie licznik odebranych bitow indacshiftregidx bedzie sie wielokrotnie przepelniac odbieraja nowe bity,
+	//W tym czasie licznik odebranych bitow conf_idx bedzie sie wielokrotnie przepelniac odbieraja nowe bity,
 	// przez co nie bedzie mozna stwierdzic ile bitow za duzo zostalo przeslanych
-	reg receivedtoomanybits = 1'b0;
-	always @(negedge SPI_SCK)
-		if(DAC_CS)
-			receivedtoomanybits <= 1'b0;
-		else if(indacshiftregidx > 32)
-			receivedtoomanybits <= 1'b1;
-	
+	wire receivedtoomanybits = (conf_idx > 32);	
 	
 	always @(negedge DAC_CS)
 		if(reseted)
@@ -150,7 +140,7 @@ module dacLTC2624behav
 						$display("%t BLAD Do daca wyslanych zostalo wiecej bitow niz 32", $time);
 				end else
 					if(LOGLEVEL >= 1)		
-						$display("%t BLAD Do daca wyslanych zostalo %d bitow. Nalezy wyslac 32", $time, indacshiftregidx-1);
+						$display("%t BLAD Do daca wyslanych zostalo %d bitow. Nalezy wyslac 32", $time, conf_idx);
 			
 			
 			end else begin
@@ -159,10 +149,10 @@ module dacLTC2624behav
 					$display("%t INFO4 Zakonczenie odbioru danych", $time);
 				if(LOGLEVEL >= 3)
 					$display("%t INFO3 ustawiono\tliczbe %d (0x%h)\tna dacu nr %d (0x%h)\tz komenda %d (0x%h)", $time, data, data, address, address, command, command);
-				
+								
 				case(address)
 					4'b0000:	if(LOGLEVEL >= 5) $display("%t INFO5 dac nr %b (0x%h) - ustawi dac A", $time, address, address);
-					4'b0001:	if(LOGLEVEL >= 5) $display("%t INFO5 dac nr %b (0x%h) - ustawi dac B", $time, address, address);
+					4'b0001: if(LOGLEVEL >= 5) $display("%t INFO5 dac nr %b (0x%h) - ustawi dac B", $time, address, address);
 					4'b0010:	if(LOGLEVEL >= 5) $display("%t INFO5 dac nr %b (0x%h) - ustawi dac C (mozliwe ustawienie wzmocnienia)", $time, address, address);
 					4'b0011:	if(LOGLEVEL >= 5) $display("%t INFO5 dac nr %b (0x%h) - ustawi dac D (mozliwe ustawienie wzmocnienia)", $time, address, address);
 					4'b1111:	if(LOGLEVEL >= 5) $display("%t INFO5 dac nr %b (0x%h) - ustawi wszystkie dac'i", $time, address, address);
@@ -179,5 +169,5 @@ module dacLTC2624behav
 	//Na linii wyjsciowej DAC_OUT beda sie pojawiac kolejne bity wypychane z rejestru przesuwnego konfigurujacego daca
 	//Nastepuje to w takt zegara SPI_SCK przy obnizonej linii aktywacji transmisji DAC_CS
 	//TODO odbieranie ostatniego bitu
-	assign DAC_OUT = DAC_CS ? 1'b0 : indacshiftreg[31];
+	assign DAC_OUT = DAC_CS ? 1'b0 : conf[31];
 endmodule
