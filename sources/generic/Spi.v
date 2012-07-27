57be54057f8d561c391cff7039ca62b814f4a8a8
Krystian Wojtas <krystian.wojtas@gmail.com>
2012-07-27 10:15:39 +0200

    todo state cs_low in spi module

--- sources/generic/Spi.v
+++ sources/generic/Spi.v
@@ -72,8 +72,9 @@ module Spi #(
 			
 	reg [1:0] state;
 	localparam [1:0] 	TRIG_WAITING = 2'd0,
-							SENDING = 2'd1,	
-							DONE = 2'd2;
+							CS_LOW = 2'd1,
+							SENDING = 2'd2,	
+							DONE = 2'd3;
 	
 	
 	always @(posedge CLK50MHZ) begin
@@ -82,9 +83,12 @@ module Spi #(
 				case(state)
 					TRIG_WAITING:
 						if(spi_trig)
+							state <= SENDING; //
+					CS_LOW: // TODO del
+						if(clk_pos_trig)
 							state <= SENDING;
 					SENDING:
-						if(clk_neg_trig & shiftreg_idx == WIDTH+1)
+						if(clk_pos_trig & shiftreg_idx == WIDTH)
 							state <= DONE;
 					DONE:
 						state <= TRIG_WAITING;
@@ -95,32 +99,19 @@ module Spi #(
 			
 	always @(posedge CLK50MHZ) begin
 		if(RST) begin
+			shiftreg <= 0;
 			shiftreg_idx <= 0;
 		end else
 			case(state)
 				TRIG_WAITING: begin
+					shiftreg <= data_in;
 					shiftreg_idx <= 0;
 				end
 				SENDING: 
-					if(clk_neg_trig)
-						if(shiftreg_idx <= WIDTH) //TODO <= ? <
-							shiftreg_idx <= shiftreg_idx + 1;
-						else
-							shiftreg_idx <= 0;
-			endcase
-	end
-			
-	always @(posedge CLK50MHZ) begin
-		if(RST) begin
-			shiftreg <= 0;
-		end else
-			case(state)
-				TRIG_WAITING:
-					shiftreg <= data_in;
-				SENDING: 
-					if(clk_neg_trig)
-						if(shiftreg_idx > 0)
-							shiftreg <= { shiftreg[WIDTH-2:0], spi_miso };
+					if(clk_pos_trig) begin
+						shiftreg <= { shiftreg[WIDTH-2:0], spi_miso };
+						shiftreg_idx <= shiftreg_idx + 1;
+					end
 			endcase
 	end
 			
@@ -147,7 +138,7 @@ module Spi #(
 				TRIG_WAITING:
 					spi_sck_en <= 1'b1;
 				SENDING:
-					if(clk_neg_trig)
+					if(clk_pos_trig)
 						if(shiftreg_idx < WIDTH) begin
 							spi_sck_en <= 1'b0;
 						end else
@@ -155,18 +146,18 @@ module Spi #(
 			endcase
 	end
 	
-			
+	
 	always @(posedge CLK50MHZ) begin
 		if(RST) spi_cs <= 1'b1;
 		else
-			if(spi_trig)
+			if(state == SENDING)
 				spi_cs <= 1'b0;
-			else if(spi_done)
-					spi_cs <= 1'b1;
+			else if(state == DONE)
+				spi_cs <= 1'b1;
 	end
 	
 	
-	assign spi_sck = (~spi_sck_en) ? clk_hf : 1'b0;			
+	assign spi_sck = ( state == SENDING & ~spi_cs) ? clk_hf : 1'b0;			
 	assign spi_done = (state == DONE);
 
 endmodule
