124beaf4b88f0be953965f4e8f21a8d835c7123f
Krystian Wojtas <krystian.wojtas@gmail.com>
2012-10-09 19:44:50 +0200

    Spi module divided into SpiCore and ModClkConditional

--- sources/generic/Spi.v
+++ sources/generic/Spi.v
@@ -27,9 +27,9 @@ module Spi #(
 	input CLK50MHZ,
 	// spi lines
 	output spi_sck,
-	output reg spi_cs,
+	output spi_cs,
 	input spi_miso,
-	output reg spi_mosi,
+	output spi_mosi,
 	// spi module interface
 	input [WIDTH-1:0] data_in,
 	output [WIDTH-1:0] data_out,
@@ -51,105 +51,26 @@ module Spi #(
 		.clk_neg_trig(clk_neg_trig)	
 	);
 	
-	 
-	//constant function calculetes value at collaboration time
-	//source http://www.beyond-circuits.com/wordpress/2008/11/constant-functions/
-	function integer log2;
-	  input integer value;
-	  begin
-		 value = value-1;
-		 for (log2=0; value>0; log2=log2+1)
-			value = value>>1;
-	  end
-	endfunction
 	
-			
-	reg [WIDTH-1:0] shiftreg;
-	reg [log2(WIDTH):0] shiftreg_idx;
-	
-	assign data_out = shiftreg;
-			
-			
-	reg [1:0] state;
-	localparam [1:0] 	TRIG_WAITING = 2'd0,
-							SENDING = 2'd1,	
-							DONE = 2'd2;
-	
-	
-	always @(posedge CLK50MHZ) begin
-		if(RST) state <= TRIG_WAITING;
-		else begin
-				case(state)
-					TRIG_WAITING:
-						if(spi_trig)
-							state <= SENDING;
-					SENDING:
-						if(clk_pos_trig & shiftreg_idx == WIDTH+1)
-							state <= DONE;
-					DONE:
-						state <= TRIG_WAITING;
-				endcase
-		end
-	end
-			
-			
-	always @(posedge CLK50MHZ) begin
-		if(RST) begin
-			shiftreg_idx <= 0;
-		end else
-			case(state)
-				TRIG_WAITING: begin
-					shiftreg_idx <= 0;
-				end
-				SENDING: 
-					if(clk_pos_trig)
-						if(shiftreg_idx <= WIDTH) //TODO <= ? <
-							shiftreg_idx <= shiftreg_idx + 1;
-						else
-							shiftreg_idx <= 0;
-			endcase
-	end
-			
-			
-	always @(posedge CLK50MHZ) begin
-		if(RST) begin
-			shiftreg <= 0;
-		end else
-			case(state)
-				TRIG_WAITING:
-					shiftreg <= data_in;
-				SENDING: 
-					if(clk_pos_trig & shiftreg_idx > 0)
-						shiftreg <= { shiftreg[WIDTH-2:0], spi_miso };
-			endcase
-	end
-			
-	
-	always @(posedge CLK50MHZ) begin
-		if(RST) begin
-			spi_mosi <= 1'b0;
-		end else
-			case(state)
-				TRIG_WAITING:
-					spi_mosi <= 1'b0;
-				SENDING:
-					if(clk_pos_trig & shiftreg_idx > 0)
-						spi_mosi <= shiftreg[WIDTH-1];
-			endcase
-	end
-	
-			
-	always @(posedge CLK50MHZ) begin
-		if(RST) spi_cs <= 1'b1;
-		else
-			if(spi_trig)
-				spi_cs <= 1'b0;
-			else if(spi_done)
-					spi_cs <= 1'b1;
-	end
-	
-	
-	assign spi_sck = (shiftreg_idx > 1 & shiftreg_idx <= WIDTH+1) ? clk_hf : 1'b0;			
-	assign spi_done = (state == DONE);
+	SpiCore #(
+		.WIDTH(WIDTH)
+	) SpiCore_ (
+		.CLK50MHZ(CLK50MHZ),
+		.RST(RST),
+		// spi lines
+		.spi_sck(spi_sck),
+		.spi_cs(spi_cs),
+		.spi_mosi(spi_mosi),
+		.spi_miso(spi_miso),
+		// spi module interface
+		.data_in(data_in),
+		.data_out(data_out),
+		.spi_trig(spi_trig),
+		.spi_done(spi_done),
+		// spi clock
+		.clk_hf(clk_hf),
+		.clk_pos_trig(clk_pos_trig),
+		.clk_neg_trig(clk_neg_trig)		
+	);
 
 endmodule
