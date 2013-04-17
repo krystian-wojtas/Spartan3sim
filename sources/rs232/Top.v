20b50ee9451e1b9ece3cae4b3e4ad99df945331c
Krystian Wojtas <krystian.wojtas@gmail.com>
2013-04-17 21:02:38 +0200

    deboucer once and leds

--- sources/rs232/Top.v
+++ sources/rs232/Top.v
@@ -30,6 +30,31 @@ module Top(
 	 
 	wire RXD= 1'b0; // TODO del
 	 
+	wire btn_debounce;
+	Counter #(
+	  .MAX(1_00) // sim
+//	  .MAX(10_000_000) // synth
+	) Debouncer_less (
+	  .CLKB(CLK50MHZ),
+	  .en(1'b1),
+	  .rst(RST),
+	  .sig(BTN),
+	  .full(btn_debounce)
+	);
+	
+	reg btn_once = 1'b0;
+	wire btn_ee = btn_debounce & ~btn_once;
+	always @(posedge CLK50MHZ)
+		if(RST)
+			btn_once <= 1'b0;
+		else if(~BTN)
+			btn_once <= 1'b0;
+		else if(btn_debounce && ~btn_once)
+			btn_once <= 1'b1;
+			
+//	reg [7:0] leds = { 3'b010, 4'h5, btn_once };
+	assign LED = { 3'b010, 4'h5, btn_once };
+	
 	 	 
 	 serialfun serialfun_(
 		.CLK50MHZ(CLK50MHZ),
