6c29e65ddd9b94679f334c6cde4be7b073f7da12
Krystian Wojtas <kwojtas@cern.ch>
2013-10-13 18:29:59 +0200

    ready signal from serial module is not synchronizes with tick anymore

--- Serial.v
+++ Serial.v
@@ -64,7 +64,7 @@ module Serial  #(
         always @(posedge CLKB)
                 if(RST)
                         ready <= 1'b1;
-                else if(sent_all_bits && tick)
+                else if(sent_all_bits)
                         ready <= 1'b1;
                 else if(trig)
                         ready <= 1'b0;
