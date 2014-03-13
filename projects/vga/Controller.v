a2d3f2bbcd0ca178c9625cb78c17249cbc483010
Krystian Wojtas <kwojtas@cern.ch>
2014-03-13 21:58:04 +0100

    No introduction comments

--- projects/vga/Controller.v
+++ projects/vga/Controller.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    19:22:34 08/18/2013
-// Design Name:
-// Module Name:    Controller
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 module Controller (
     input      CLK50MHZ,
     input      RST,
