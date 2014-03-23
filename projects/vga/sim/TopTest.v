d9060c3f9621cac2595511182068e828ad2bf302
Krystian Wojtas <kwojtas@cern.ch>
2014-03-23 11:47:29 +0100

    vga synt no introtuction comments

--- projects/vga/sim/TopTest.v
+++ projects/vga/sim/TopTest.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    19:17:46 08/18/2013
-// Design Name:
-// Module Name:    TopTest
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 module TopTest ();
 
--- projects/vga/sim/TopTestBench.v
+++ projects/vga/sim/TopTestBench.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    19:18:08 08/18/2013
-// Design Name:
-// Module Name:    TopTestBench
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 module TopTestBench (
     // color control
     output reg BTN_NEXT = 1'b0,
