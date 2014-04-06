603f9948cf6b4acc88957075089b2c3cdb3e1069
Krystian Wojtas <kwojtas@cern.ch>
2014-04-06 21:43:35 +0200

    generic counter synt introduction comments, timescale

--- projects/generic/Counter.v
+++ projects/generic/Counter.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    19:51:49 07/22/2012
-// Design Name:
-// Module Name:    Counter
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 module Counter #(
         parameter MAX=4,
         parameter K=1,
