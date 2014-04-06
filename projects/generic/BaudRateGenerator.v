c66bf78de969470dea4083cd0e3ed462226422f5
Krystian Wojtas <kwojtas@cern.ch>
2014-04-06 11:43:07 +0200

    whitespaces

--- projects/generic/BaudRateGenerator.v
+++ projects/generic/BaudRateGenerator.v
@@ -1,19 +1,19 @@
 module BaudRateGenerator
 #(
-	parameter INC = 100,
+        parameter INC = 100,
         parameter N = 10
 ) (
-	input CLK50MHZ,
-	input RST,
-	input en,
-	output tick
+        input CLK50MHZ,
+        input RST,
+        input en,
+        output tick
 );
 
-	reg [N:0] acc = {N{1'b0}};
-	always @(posedge CLK50MHZ)
-		if(RST) acc <= {N{1'b0}};
-		else if(en) acc <= acc[N-1:0] + INC;
+        reg [N:0] acc = {N{1'b0}};
+        always @(posedge CLK50MHZ)
+                if(RST) acc <= {N{1'b0}};
+                else if(en) acc <= acc[N-1:0] + INC;
 
-	assign tick = acc[N];
+        assign tick = acc[N];
 
 endmodule
--- projects/generic/Counter.v
+++ projects/generic/Counter.v
@@ -20,36 +20,36 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module Counter #(
-	parameter MAX=4,
-	parameter K=1,
-	parameter DELAY=0
+        parameter MAX=4,
+        parameter K=1,
+        parameter DELAY=0
 ) (
-	input 	      CLKB,
-	// counter
-	input 	      en, // if high counter is enabled and is counting
-	input 	      rst, // set counter register to zero
-	input 	      sig, // signal which is counted
+        input         CLKB,
+        // counter
+        input         en, // if high counter is enabled and is counting
+        input         rst, // set counter register to zero
+        input         sig, // signal which is counted
 // TODO log2
-	output [10:0] cnt,
-	output reg    full // one pulse if counter is full
+        output [10:0] cnt,
+        output reg    full // one pulse if counter is full
 );
 
 `include "log2.v"
 
-	reg [log2(MAX):0] counter_reg = 0;
-	always @(posedge CLKB)
-		if(rst)
-			counter_reg <= 0;
-		else if(en & sig)
-			if(counter_reg < MAX)
-				counter_reg <= counter_reg + K;
-			else
-				counter_reg <= DELAY;
+        reg [log2(MAX):0] counter_reg = 0;
+        always @(posedge CLKB)
+                if(rst)
+                        counter_reg <= 0;
+                else if(en & sig)
+                        if(counter_reg < MAX)
+                                counter_reg <= counter_reg + K;
+                        else
+                                counter_reg <= DELAY;
 
-	//assign full = (counter_reg == MAX-1);
-	always @*
-		full = (counter_reg == MAX);
+        //assign full = (counter_reg == MAX-1);
+        always @*
+                full = (counter_reg == MAX);
 
-	assign cnt = counter_reg;
+        assign cnt = counter_reg;
 
 endmodule
